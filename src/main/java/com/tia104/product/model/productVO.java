f4403747760c22a781976a03404fcbf9bf955c93
Tom <lawrencefish35@gmail.com>
2024-11-30 15:28:13 +0800

    add product bean java file

---
+++ src/main/java/com/tia104/product/model/productVO.java
@@ -0,0 +1,21 @@
+package com.tia104.product.model;
+
+public class productVO {
+	private Integer prdId;
+	private String prdName;
+	
+	public Integer getPrdId() {
+		return prdId;
+	}
+	public void setPrdId(Integer prdId) {
+		this.prdId = prdId;
+	}
+	public String getPrdName() {
+		return prdName;
+	}
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+	
+	
+}
