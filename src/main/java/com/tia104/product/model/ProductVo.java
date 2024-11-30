03060ec53f584bb0464c5205e1e3f0e09bdb8d41
Louis Chang <zu5462@gmail.com>
2024-11-30 15:53:56 +0800

    push

--- src/main/java/com/tia104/product/model/ProductVo.java
+++ src/main/java/com/tia104/product/model/ProductVo.java
@@ -1,7 +1,21 @@
 package com.tia104.product.model;
 
-public class ProductVo {
-	public static void main(String[] arge) {
-		System.out.println("HelloWorld!");
+public class productVO {
+	private Integer prdId;
+	private String prdName;
+	
+	public Integer getPrdId() {
+		return prdId;
 	}
+	public void setPrdId(Integer prdId) {
+		this.prdId = prdId;
+	}
+	public String getPrdName() {
+		return prdName;
+	}
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+	
+	
 }
