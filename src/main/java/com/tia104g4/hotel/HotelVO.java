1d44158944316a1d75ef0cff2091689669015a70
Louis Chang <louis54622@gmail.com>
2024-11-30 16:22:23 +0800

    4th commit

--- src/main/java/com/tia104g4/hotel/HotelVO.java
+++ src/main/java/com/tia104g4/hotel/HotelVO.java
@@ -2,8 +2,8 @@ package com.tia104g4.hotel;
 
 public class HotelVO {
 	private Integer hotelID;
-	private String hotelLoc;
 	private String hotelName;
+
 	
 	public Integer getHotelID() {
 		return hotelID;
