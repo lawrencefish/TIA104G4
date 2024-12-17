993add6c57b051131c13322990b34a0548ca0f2f
lawrencefish <lawrencefish35@gmail.com>
2024-12-17 20:29:27 +0800

    delete all

--- src/main/java/com/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("早安世界你好！~");
-	}
-}
--- src/main/java/com/hehe.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-public class hehe {
-	public static void main(String[] args) {
-		System.out.println("hhee");
-	}
-}
